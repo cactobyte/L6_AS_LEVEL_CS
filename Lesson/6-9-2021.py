12bef05e1a3867362f34cc19b9e7413e99c1885e
cm <54995353+cactobyte@users.noreply.github.com>
2021-09-06 18:33:29 +0100

    Update 6-9-2021.py

--- Lesson/6-9-2021.py
+++ Lesson/6-9-2021.py
@@ -1,1 +1,22 @@
+# Psuedo Code Example
 
+# a = 3
+# b = 4
+# c = b
+# b = a
+# a = c
+#
+# print(a, b)
+#
+# print(id(a))
+
+# #Assignment Example
+# name = "Albert"
+# sentence = "The quick brown fox jumped over the lazy dogs"
+# d = 5
+# e = 3.141592653589793238462648838279
+# f = True
+# big = 2**64
+
+# import sys
+# print(sys.float_info) #Displays info about Max Float
