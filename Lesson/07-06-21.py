ea498b25650a82cd9ea620a2d395dd6a02a0fbfd
cm <54995353+cactobyte@users.noreply.github.com>
2021-09-07 12:42:47 +0100

    Create 07-06-21.py

---
+++ Lesson/07-06-21.py
@@ -0,0 +1,23 @@
+# 07/09
+# Assignment Starter
+
+# x = 3
+# y = 5
+#
+# x = x + y
+# y = x - y
+# x = x - y
+# print(x, y)
+
+# Array: Example
+# names = ["Boris", "Oscar"]
+
+# for name in names:
+#     print(name) # prints all names
+#
+# print(names[0])
+
+name = ["Boris", ["22222", "iwajdoiaj"], "Keir", "Albert", "Michael"]
+print(name[1])
+print(name[1][0]) # Nested List within List
+
