3d71da5ec28a3a96796ab7571c8bab7acbf34bc1
cm <54995353+cactobyte@users.noreply.github.com>
2021-09-08 14:53:34 +0100

    Update 08-09-2021.py
    If statements (Elif Else)

--- Lesson/08-09-2021.py
+++ Lesson/08-09-2021.py
@@ -6,3 +6,16 @@ else:
     print("{}, you are {} years away from earning a bus pass".format(name, 65-age))
 
 #
+
+amount = int(input())
+if(amount<500):
+    rate = 0.01
+elif(amount < 3000):
+    rate = 0.015
+elif(amount < 10000):
+    rate = 0.02
+else:
+    rate = 0.035
+
+print(rate)
+
