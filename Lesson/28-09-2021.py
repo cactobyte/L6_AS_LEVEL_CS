f7a7b8ea09d1733e8c7292afdda166c9a1c2b8c9
cm <54995353+cactobyte@users.noreply.github.com>
2021-09-28 12:36:44 +0100

    Create 28-09-2021.py

---
+++ Lesson/28-09-2021.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        f = 1
+        for i in range(2, n+1):
+            f *= i
+    return f
+
+print(factorial(2))
+print(factorial(3))
+print(factorial(4))
+# 
+# import math
+# print(math.factorial(2),math.factorial(3),math.factorial(4))
