1a35546daae9cd6bf2d0ba436b5a53561888ced2
cm <54995353+cactobyte@users.noreply.github.com>
2021-10-01 11:36:36 +0100

    Update 01-10-2021.py

--- Prep/01-10-2021.py
+++ Prep/01-10-2021.py
@@ -16,3 +16,13 @@ for i in weight:
 lowAverage = y / z
 print("The low average is: ", lowAverage)
 
+
+### q3
+
+mark = [[4, 0], [3, 1], [3, 2], [5, 3], [1, 4]]
+
+for x in range(0, 5):
+    studentMark = int(input("Please enter the score that student {} achieved".format(x+1)))
+    mark.append([studentMark, x])
+
+### q4
