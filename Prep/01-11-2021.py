4da8601a36f8b5bcaf01436cfa736815a42b7ceb
cm <54995353+cactobyte@users.noreply.github.com>
2021-11-01 22:00:29 +0000

    binary counter prep (reverse in prog)

--- Prep/01-11-2021.py
+++ Prep/01-11-2021.py
@@ -1,1 +1,11 @@
-a
+def strToBinary(binthing):
+    bin = list(binthing)
+
+    binary = len(bin) - 1 #3
+    total, power = 0, 0
+    
+    for i in reversed(range(0, binary+1)):
+        if bin[i] == "1":
+            total = total + int(bin[i])*2**power
+        power += 1
+    return(total)
