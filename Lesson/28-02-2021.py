91ab85fded66b0d08e975b4f15a748b731f681e9
cm <54995353+cactobyte@users.noreply.github.com>
2022-02-28 10:32:25 +0000

    bruh

---
+++ Lesson/28-02-2021.py
@@ -0,0 +1,20 @@
+string = str(input("input>> "))
+res = ""
+count = 1
+
+res += string[0]
+
+
+for i in range(len(string)-1):
+    if string[i] == string[i+1]:
+        count += 1
+    else:
+        if count > 1:
+            res += str(count)
+        res += string[i+1]
+        count = 1
+
+if count > 1:
+    res += str(count)
+
+print(res)
